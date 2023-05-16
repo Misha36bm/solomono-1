09e1f063aabf86fa8122532203d374db19a06e85
Misha36bm <47403281+Misha36bm@users.noreply.github.com>
2023-05-17 00:24:55 +0300

    config of project
    add file with config value for project
    
    create function for easer work with config file and it value

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -1,2 +1,50 @@
 <?php
 
+if (!function_exists('config')) {
+
+    /**
+     * This function for geting values from config file
+     * 
+     * @param string $key key/keys for search values in config file
+     * @return mixed
+     */
+    function config(string $key)
+    {
+        $config = include __DIR__ . '/../config.php';
+
+        $keys = explode('.', $key);
+
+        $configData = $key;
+
+        foreach ($keys as $key) {
+            if (is_string($configData)) {
+                $configData = $config[$key];
+            } else {
+                $configData = $configData[$key];
+            }
+        }
+
+
+        return $configData;
+    }
+}
+
+if (!function_exists('dd')) {
+
+    /**
+     * Dev function for "dump and die"
+     * 
+     * @return never
+     */
+    function dd(mixed ...$data)
+    {
+        echo '<pre>';
+        if (count($data) == 1) {
+            var_export(array_pop($data));
+        } else {
+            var_export($data);
+        }
+        echo '</pre>';
+        die;
+    }
+}
