a290b34742f129930f9597203171bd0d8a7892e3
Misha36bm <47403281+Misha36bm@users.noreply.github.com>
2023-05-17 00:49:14 +0300

    connection to database in DB class

--- src/Database/DB.php
+++ src/Database/DB.php
@@ -2,14 +2,25 @@
 
 namespace Solomono\Tz1\Database;
 
+
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 class DB
 {
     private static $instance;
 
+    private $capsule;
+
     private function __construct()
     {
+        $capsule = new Capsule();
+
+        $capsule->addConnection(config('db-conncet'));
+
+        $capsule->setAsGlobal();
+        $capsule->bootEloquent();
+
+        $this->capsule = $capsule;
     }
 
     public static function getInstance()
@@ -21,7 +32,11 @@ class DB
         return self::$instance;
     }
 
-    public function connect()
+    /**
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public static function table(string $tableName)
     {
+        return self::getInstance()->capsule->table($tableName);
     }
 }
