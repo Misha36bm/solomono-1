e8bc62f51f719e03394e913338f7ec06a361bb36
Misha36bm <47403281+Misha36bm@users.noreply.github.com>
2023-05-18 00:38:30 +0300

    update Category model
    remove Model class extending
    
    add functions for geting categories

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -2,8 +2,26 @@
 
 namespace Solomono\Tz1\Models;
 
-use Solomono\Tz1\Database\Model;
+use Solomono\Tz1\Database\DB;
 
-class Category extends Model
+class Category
 {
+    protected static string $tableName = 'categories';
+    protected static $table;
+
+    public function __construct()
+    {
+        self::$table = self::getTable();
+    }
+
+    public static function getTable()
+    {
+        return DB::table(self::$tableName);
+    }
+
+    public static function getCategories()
+    {
+        return self::getTable()->get();
+    }
+
 }
