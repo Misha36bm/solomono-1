9808c8baedc5fe2997544cd9522b6f411241bcfb
Misha36bm <47403281+Misha36bm@users.noreply.github.com>
2023-05-17 23:06:29 +0300

    add seedes to databes seeder class

--- src/Database/Seeders/DatabaseSeeder.php
+++ src/Database/Seeders/DatabaseSeeder.php
@@ -1,1 +1,15 @@
 <?php
+
+namespace Solomono\Tz1\Database\Seeders;
+
+
+use Solomono\Tz1\Database\Seeders\{CategoriesSeeder, ProductsSeeder};
+
+class DatabaseSeeder
+{
+    public static function run()
+    {
+        CategoriesSeeder::run();
+        ProductsSeeder::run();
+    }
+}
