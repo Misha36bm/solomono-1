e63fc1b06434b70fcfb3280ce20bd9e254e175e2
Misha36bm <47403281+Misha36bm@users.noreply.github.com>
2023-05-18 01:14:08 +0300

    add modal to products template

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -79,12 +79,29 @@ if (!function_exists('product_card_template')) {
     function product_card_template($product)
     {
         return "
-        <div class=\"card-block mb-4 col-4\">
+        <div class=\"card-block mb-4 col-4\" data-bs-toggle=\"modal\" data-bs-target=\"#productModal$product->id\">
             <div class=\"card\">
                 <div class=\"card-body text-center\">
                     <h5 class=\"card-title\">$product->name</h5>
-                    <h6 class=\"card-subtitle mb-2 text-body-secondary\">$product->price</h6>
-                    <p class=\"card-text\">$product->date</p>
+                </div>
+            </div>
+        </div>
+
+        <div class=\"modal fade\" id=\"productModal$product->id\" tabindex=\"-1\" aria-labelledby=\"productModal$product->id\Label\" aria-hidden=\"true\">
+            <div class=\"modal-dialog\">
+                <div class=\"modal-content\">
+                <div class=\"modal-header\">
+                    <h1 class=\"modal-title fs-5\" id=\"productModal$product->id\Label\">$product->name</h1>
+                    <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"modal\" aria-label=\"Close\"></button>
+                </div>
+                <div class=\"modal-body\">
+                    <p>$product->price</p>
+                    <p>$product->date</p>
+                </div>
+                <div class=\"modal-footer\">
+                    <button type=\"button\" class=\"btn btn-secondary\" data-bs-dismiss=\"modal\">Close</button>
+                    <button type=\"button\" class=\"btn btn-primary\">Save changes</button>
+                </div>
                 </div>
             </div>
         </div>
