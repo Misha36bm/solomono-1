67c6216805bdd6c0bd2748209d683f12cdd29cd0
Misha36bm <47403281+Misha36bm@users.noreply.github.com>
2023-05-18 01:07:10 +0300

    update Product model
    remove Model class extending
    
    add functions for geting products

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -2,8 +2,25 @@
 
 namespace Solomono\Tz1\Models;
 
-use Solomono\Tz1\Database\Model;
+use Solomono\Tz1\Database\DB;
 
-class Product extends Model
+class Product
 {
+    protected static string $tableName = 'products';
+    protected static $table;
+
+    public function __construct()
+    {
+        self::$table = self::getTable();
+    }
+
+    public static function getTable()
+    {
+        return DB::table(self::$tableName);
+    }
+
+    public static function getProducts()
+    {
+        return self::getTable()->get();
+    }
 }
