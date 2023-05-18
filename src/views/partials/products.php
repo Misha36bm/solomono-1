42e61210187818c2ba8a2f56e97dbe8b0e54a697
Misha36bm <47403281+Misha36bm@users.noreply.github.com>
2023-05-18 03:36:03 +0300

    fix check of get params to selectors

--- src/views/partials/products.php
+++ src/views/partials/products.php
@@ -1,14 +1,41 @@
 <div class="col-7 offset-1">
     <?php
         $products = get_product_model()->getProducts();
-        $product = $products->first();
+
+        if ($_GET) {
+            if (isset($_GET['sortBy'])) {
+                switch ($_GET['sortBy']) {
+                    case 'name':
+                        $products = $products->sortBy('name');
+                        break;
+                    case 'date':
+                        $products = $products->sortBy('date')->reverse();
+                        break;
+                    case 'lower_price':
+                        $products = $products->sortBy('price');
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            if (isset($_GET['changeCategory'])) {
+                $products = $products->where('category_id', $_GET['changeCategory']);
+            }
+        }
     ?>
 
     <div class="row d-flex justify-content-between" id="products-block">
         <?php
-            foreach ($products as $product) {
-                echo product_card_template($product);
+            if (!$products->isEmpty()) {
+                foreach ($products as $product) {
+                    echo product_card_template($product);
+                }
+            } else {
+                echo '<h1>404</h1>';
             }
         ?>
+
     </div>
 </div>
