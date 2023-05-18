8ff7ed139d691d13b12d93420d6ac1a91481c03c
Misha36bm <47403281+Misha36bm@users.noreply.github.com>
2023-05-18 03:29:16 +0300

    add check of get params to selectors

--- src/views/partials/aside-menu.php
+++ src/views/partials/aside-menu.php
@@ -2,19 +2,19 @@
     <select class="form-select text-center" id="category-select">
         <option selected disabled>Select category</option>
         <?php
-            $categories = get_category_model()->getCategories();
+        $categories = get_category_model()->getCategories();
 
-            $categories->each(function ($category, $index) {
-                $isActive = false ? 'selected' : '';
+        $categories->each(function ($category, $index) {
+            $isActive = $_GET['changeCategory'] == $category->id ? 'selected' : '';
 
-                echo '<option value="' . $category->id . '" ' . $isActive . '>' . $category->name . '</option>';
-            });
+            echo '<option value="' . $category->id . '" ' . $isActive . '>' . $category->name . '</option>';
+        });
         ?>
     </select>
 
     <select class="form-select mt-5 text-center" id="sort-by-select">
-        <option value="lower_price">спочатку дешевші</option>
-        <option value="name">по алфавіту</option>
-        <option value="date">спочатку нові</option>
+        <option value="lower_price" <?= $_GET['sortBy'] == 'lower_price' ? 'selected' : '' ?>>спочатку дешевші</option>
+        <option value="name" <?= $_GET['sortBy'] == 'name' ? 'selected' : '' ?>>по алфавіту</option>
+        <option value="date" <?= $_GET['sortBy'] == 'date' ? 'selected' : '' ?>>спочатку нові</option>
     </select>
 </div>
